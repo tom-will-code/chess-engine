03c4107f073c9547c6151c69d188a6c0e7bd0f14
Thomas Williams <thomas.ev.williams.2003@gmail.com>
2026-02-03 16:38:29 +1100

    Fix a bug in legal move checks and add some bishop move logic

--- src/moves.py
+++ src/moves.py
@@ -5,14 +5,23 @@ def is_legal_move(board_state, start_sqr, end_sqr, is_whites_move):
     moving_piece = board_state[start_row][start_col]
     target = board_state[end_row][end_col]
 
+
     if start_sqr == end_sqr: # stops moves from the same square to the same square 
         return False
-    elif not target: # runs if target square is empty
-        return True
-    elif moving_piece.isupper() == target.isupper(): # stops pieces taking pieces of the same colour
+    elif target and moving_piece.isupper() == target.isupper(): # stops pieces taking pieces of the same colour
         return False
     else:
-        return True
-    
+        moving_piece = moving_piece.lower()
+        # Bishop movement rules
+        if moving_piece == 'b':
+            row_dif = abs(end_row - start_row)
+            col_dif = abs(end_col - start_col)
+            if col_dif == row_dif:
+                return True
+            else:
+                return False   
+        else:
+            return True
+
     
     
